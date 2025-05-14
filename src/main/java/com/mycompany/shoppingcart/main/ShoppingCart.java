a514ff34bedbe6609528bd7555e47a6d956758f7
unknown <evers.uribeq@autonoma.edu.co>
2025-05-14 17:14:32 -0500

    primer commit

---
+++ src/main/java/com/mycompany/shoppingcart/main/ShoppingCart.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.shoppingcart.main;
+
+/**
+ *
+ * @author ACER
+ */
+public class ShoppingCart {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
